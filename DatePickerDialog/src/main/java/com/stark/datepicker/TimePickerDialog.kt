4a7eb7ad4a044827d36b1085a508e07ac7412232
jihongwen <jhwing@163.com>
2018-11-06 15:31:21 +0800

    添加dialog样式

--- DatePickerDialog/src/main/java/com/stark/datepicker/TimePickerDialog.kt
+++ DatePickerDialog/src/main/java/com/stark/datepicker/TimePickerDialog.kt
@@ -4,24 +4,43 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
-import android.view.View
+import android.view.*
 import org.joda.time.DateTime
 
 class TimePickerDialog : DialogFragment() {
 
+    var mGravity = Gravity.BOTTOM
+    var mWidth = WindowManager.LayoutParams.MATCH_PARENT
+    var mHeight = WindowManager.LayoutParams.WRAP_CONTENT
+
     private lateinit var timePicker: TimePickerView
     private var mBeginDate: DateTime = DateTime()
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val dialogView = View.inflate(context, R.layout.time_picker_dialog, null)
-        timePicker = dialogView.findViewById(R.id.timePickerView)
-        return AlertDialog.Builder(context)
-                .setView(dialogView)
-                .create()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NO_TITLE, R.style.DatePicker_Base_Dialog)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.time_picker_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        timePicker = view.findViewById(R.id.timePickerView)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         timePicker.setCurrentTime(mBeginDate)
+
+        val window = dialog.window
+        window!!.setGravity(mGravity)
+        val params = window.attributes
+        params.height = mHeight
+        params.width = mWidth
+        window.attributes = params
+        window.addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+        window.decorView.setPadding(0, 0, 0, 0)
     }
 }
